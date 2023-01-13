150e6a5a2a3448df5f253da9ba815246f292da6e
cardon <philantrope1@outlook.com>
2023-01-13 16:35:39 +0100

    validations and relation bdd

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :products
+  has_many :bought_products, trought: :post, dependent: :destroy
+
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :addresse, presence: true
+  validates :age, optional: true
 end
