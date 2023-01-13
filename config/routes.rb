1eda87054ee9f4dc0ff6427584e565d117c9bce4
cardon <philantrope1@outlook.com>
2023-01-13 16:10:32 +0100

    route and controller homepage and view homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'homepages#home'
+  #get 'homepages/index'
+  #get 'homepages/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :products, only: %i[index show update destroy] do
+    resources :posts, only: [:create]
+  end
 end
